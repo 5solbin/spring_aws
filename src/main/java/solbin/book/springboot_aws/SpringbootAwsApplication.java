5c3501cf2ece9759040e424039030faa76e360c7
5solbin <dhrhfqls@naver.com>
2025-06-24 09:53:24 +0900

    3장

--- src/main/java/solbin/book/springboot_aws/SpringbootAwsApplication.java
+++ src/main/java/solbin/book/springboot_aws/SpringbootAwsApplication.java
@@ -1,9 +1,13 @@
 package solbin.book.springboot_aws;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.SpringBootConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
-@SpringBootApplication
+@EnableJpaAuditing
+@SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
 public class SpringbootAwsApplication {
 
     public static void main(String[] args) {
